4ac25c483e33fad91c619e5fef3e41a454f71021
natsumi <natsu.bsk810@gmail.com>
2024-06-30 16:54:43 +0900

    fix

--- db/migrate/20240601063222_add_character_id_to_pictures.rb
+++ db/migrate/20240601063222_add_character_id_to_pictures.rb
@@ -2,6 +2,6 @@ class AddCharacterIdToPictures < ActiveRecord::Migration[7.1]
   def change
     # add_column :pictures, :character_id, :integer
     # add_index :pictures, :character_id
-    add_foreign_key :pictures, :character_id
+    # add_foreign_key :pictures, :character_id
   end
 end
